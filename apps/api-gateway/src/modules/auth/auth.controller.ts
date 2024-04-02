6fbc622d9f385a7170905420fd338a4bfef4b4fd
Boris Muñoz <bmunoz89@users.noreply.github.com>
2024-04-02 12:36:57 -0300

    type error

--- apps/api-gateway/src/modules/auth/auth.controller.ts
+++ apps/api-gateway/src/modules/auth/auth.controller.ts
@@ -16,7 +16,9 @@ export class AuthController {
 
   @Post('sign-up')
   async signUp(@Body(ValidationPipe) createUserDto: CreateUserDto) {
-    const user = await this.authService.getUserByEmail(createUserDto.email);
+    const user: { name: string } = await this.authService.getUserByEmail(
+      createUserDto.email
+    );
     if (user) {
       throw new ConflictException('User already exist');
     }
